36f276debdf19b5d0dd8c74e4c343abac7d16487
codesareus <125232658+codesareus@users.noreply.github.com>
2025-02-19 10:09:26 -0600

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -20,22 +20,21 @@ from matplotlib.lines import Line2D
 import pandas_market_calendars as mcal
 #import pygame
 import base64
-from mplayer import play_music
 
 music = ['1.mp3', '2.mp3', '3.mp3', '4.mp3']
 
-#def play_music(number=0):
-    # Assuming 'music' is a list of file paths to your audio files
-    #audio_file = open(music[number], "rb")
-   # audio_bytes = audio_file.read()
-    #audio_base64 = base64.b64encode(audio_bytes).decode('utf-8')
+def play_music(number=0):
+    Assuming 'music' is a list of file paths to your audio files
+    audio_file = open(music[number], "rb")
+    audio_bytes = audio_file.read()
+    audio_base64 = base64.b64encode(audio_bytes).decode('utf-8')
     
     # Remove the 'muted' attribute to allow sound
-    #audio_html = f"""
-       # <audio controls autoplay>
-            #<source src="data:audio/wav;base64,{audio_base64}" type="audio/wav">
-      #  </audio>
-    #"""
+    audio_html = f"""
+        <audio controls autoplay>
+            <source src="data:audio/wav;base64,{audio_base64}" type="audio/wav">
+        </audio>
+    """
     #st.markdown(audio_html, unsafe_allow_html=True)
 
 #midwest = pytz.timezone("America/New")
