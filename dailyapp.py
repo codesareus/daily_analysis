ce2a781b470ad5aef0b95e75c1bd6931138d0e6e
codesareus <125232658+codesareus@users.noreply.github.com>
2025-02-24 14:26:18 -0600

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -1079,8 +1079,11 @@ def main():
     st.write(f"{message}")
     
     ########## B and S actions
-    if "pl" not in st.session_state:
-       st.session_state.pl = 0
+    pl=0
+    if st.session_state.sb_status==1:
+        pl=current_price-st.session_state.temp_price
+    elif st.session_state.sb_status==-1:
+        pl=-current_price+st.session_state.temp_price
     
     def save_pe(SB= "", price=None, total =0):      
         pl=0
@@ -1127,8 +1130,7 @@ def main():
                     "pl": round(pl, 2),
                     "total": round(total, 2)
                 }])
-        st.session_state.pl = pl
-        st.write(f"current_p:{st.session_state.pl}")
+        st.session_state.type = SB
         # Append to CSV file
         new_data.to_csv(pe_file, mode="a", header=False, index=False)
             
