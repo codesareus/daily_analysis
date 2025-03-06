ecf7eec18615eb624e99a28370c0e3fdc74a4355
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-05 18:29:02 -0600

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -1581,27 +1581,27 @@ def main():
         ### run automatic SB
         total = updated_data["total"].iloc[-1]
         SB = updated_data["type"].iloc[-1]
-        if (b_condition or (current_price <= st.session_state.setpr and st.session_state.settype =="B")) and (SB == "AAA" or SB == "S" or SB == "SB") and intervals[st.session_state.index] == "1m":
-            save_pe("B", current_price, total)
-            st.session_state.temp_price = current_price
-            st.write(f"B: Yes ||SB_status: {SB}")
+        #if (b_condition or (current_price <= st.session_state.setpr and st.session_state.settype =="B")) and (SB == "AAA" or SB == "S" or SB == "SB") and intervals[st.session_state.index] == "1m":
+         #   save_pe("B", current_price, total)
+           # st.session_state.temp_price = current_price
+           # st.write(f"B: Yes ||SB_status: {SB}")
                   
-        elif (s_condition or current_price >= st.session_state.setpr) and SB == "B" and intervals[st.session_state.index] == "1m":
-            save_pe("S", current_price, total)
-            st.session_state.temp_price = 0
-            st.write(f"S: Yes ||SB_status: {SB}")
+       # elif (s_condition or current_price >= st.session_state.setpr) and SB == "B" and intervals[st.session_state.index] == "1m":
+          #  save_pe("S", current_price, total)
+          #  st.session_state.temp_price = 0
+           # st.write(f"S: Yes ||SB_status: {SB}")
     
-        elif (short_s or (current_price >= st.session_state.setpr and st.session_state.settype =="SS")) and (SB == "AAA" or SB == "S" or SB== "SB") and intervals[st.session_state.index] == "1m":
-            save_pe("SS", current_price, total)
-            st.session_state.temp_price = current_price
-            st.write(f"SS: Yes ||SB_status: {SB}")
+        #elif (short_s or (current_price >= st.session_state.setpr and st.session_state.settype =="SS")) and (SB == "AAA" or SB == "S" or SB== "SB") and intervals[st.session_state.index] == "1m":
+        #    save_pe("SS", current_price, total)
+         #   st.session_state.temp_price = current_price
+          #  st.write(f"SS: Yes ||SB_status: {SB}")
     
-        elif (short_b or current_price <= st.session_state.setpr) and SB == "SS" and intervals[st.session_state.index] == "1m":
-            save_pe("SB", current_price, total)
-            st.session_state.temp_price = 0
-            st.write(f"SB: Yes ||SB_status: {SB}")
+        #elif (short_b or current_price <= st.session_state.setpr) and SB == "SS" and intervals[st.session_state.index] == "1m":
+            #save_pe("SB", current_price, total)
+            #st.session_state.temp_price = 0
+            #st.write(f"SB: Yes ||SB_status: {SB}")
 
-        st.write(f"B: {b_condition}__SS: {short_s}__S: {s_condition}__SB:{short_b}__Status_0: {SB == "AAA" or SB == "S" or SB == "SB"}__interval: {intervals[st.session_state.index]}")
+        #st.write(f"B: {b_condition}__SS: {short_s}__S: {s_condition}__SB:{short_b}__Status_0: {SB == "AAA" or SB == "S" or SB == "SB"}__interval: {intervals[st.session_state.index]}")
        # st.empty()
         st.rerun()
         
