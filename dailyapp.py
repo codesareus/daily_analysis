61fa0a10234578bcb1cca01ab5f30f6631acaf2e
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-06 15:42:42 -0600

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -920,7 +920,7 @@ def main():
    ## read bar data scoreT_fil
     fig, (ax0) = plt.subplots(1, 1, figsize=(12, 6))
     
-    df = pd.read_csv(scoreT.csv, names=['tFrame', 'ema_trend', 'ema', 'rsi', 'macd', 'total', 'dev_from_std', "y_pred_p_trend", 'score_trend'])
+    df = pd.read_csv(scoreT_file, names=['tFrame', 'ema_trend', 'ema', 'rsi', 'macd', 'total', 'dev_from_std', "y_pred_p_trend", 'score_trend'])
     
     # Define custom order
     timeframe_order = ["1m", "5m", "15m", "30m", "1h", "3mo", "6mo"]
