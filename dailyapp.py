c7874ea16c310f44b9a3d18b2ef1deb4dd2e1296
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-05 18:27:49 -0600

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -19,7 +19,6 @@ from datetime import datetime, time
 from time import sleep
 from matplotlib.lines import Line2D
 import pandas_market_calendars as mcal
-import td
 
 #eastern = pytz.timezone("America/New")
 eastern = pytz.timezone("US/Eastern")
@@ -319,9 +318,7 @@ def main():
 
     # Initialize sbOK state
     #if "sbOK" not in st.session_state:
-        #st.session_state.sbOK = 
-    td
-
+        #st.session_state.sbOK 
 
     # Define file names
     
