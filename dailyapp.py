d81c51f61dbf0b3636f7580bf351f44b8c1895a1
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-06 10:48:29 -0600

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -1239,7 +1239,7 @@ def main():
     plt.figure(figsize=(6, 3))
     plt.xticks(rotation=45)
     plt.plot(x_values, y, color="black", label="Actual Prices")  # Ac
-    plt.title("Stock Closing Price Over Time")
+    plt.title(f"{interval}__{now}")
     plt.xlabel("Time")
     plt.ylabel("Close Price")
     plt.legend()
