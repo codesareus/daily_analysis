a4c29f1acf2f5a54a8297406529e2ec5e75bb243
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-07 07:42:24 -0600

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -618,7 +618,10 @@ def main():
         else:
             ema_trend = 0
         
-        ema_score = ((price > ema9) + (ema9 > ema20) +  (ema100 > ema200)) - ((ema200 > ema100) + (ema20 > ema9) + (ema9 > price))
+        ema_score = (
+            (price > ema9) + (ema9 > ema20) + (ema100 > ema200) -
+            (price < ema9) - (ema9 < ema20) - (ema100 < ema200)
+        )
             
         rsi_score = 0
         
