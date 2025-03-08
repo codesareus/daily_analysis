45c4941f827c539123ee23e9add3f6ba6d5a10be
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-08 08:14:28 -0600

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -692,11 +692,22 @@ def main():
         )
     # draw high 
     for i in range(1, len(x_values)):
-        ax.vlines(x_values[i], high[i], max(open[i], close[i]), color="black", linewidth=1, label="High-Low Range")
+        ax.vlines(x_values[i], 
+            high[i], 
+            max(open[i], close[i]), 
+            color="black", 
+            linewidth=1, 
+            label="High-Low Range"
+        )
 
     #draw low   
     for i in range(1, len(x_values)):
-        ax.vlines(x_values[i], low[i], min(open[i], close[i]), color="black", linewidth=1, label="High-Low Range")
+        ax.vlines(x_values[i], 
+                  low[i], min(open[i], close[i]), 
+                  color="black", 
+                  linewidth=1, 
+                  label="High-Low Range"
+                 )
 #ax.plot(x_values, y, color="black", label="Actual Prices")  # Actual prices as a gray line plot
     ax.plot(x_values, y_pred_linear, color="red", label=f"L.R. (R² = {r2_linear:.2f})")
     ax.plot(x_values, y_pred_poly, color="purple", linewidth=3, label=f"P.R. (d {degree}, R² = {r2_poly:.2f})")
