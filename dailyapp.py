26bf5011306efec2263c91c23411e8a21bee5136
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-08 09:44:12 -0600

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -712,9 +712,10 @@ def main():
 
     # draw ema vaerage 
     
-    emaavg = data_recent["EMA_9"]
+    emaavg = []
     for i in range(1, len(x_values)):
         emaavg[i] = (data_recent["EMA_9"][i] + data_recent["EMA_20"][i] + data_recent["EMA_50"][i]+data_recent["EMA_100"][i]+data_recent["EMA_200"][i])/5
+        emaavg.append(emaavg[i])
     st.write(emaavg[-5:])
     
     
