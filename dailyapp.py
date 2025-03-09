dae9b89d69976c809b7cbf3eb8aa6e6feac1efd0
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-08 18:11:07 -0600

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -327,6 +327,7 @@ def main():
     # Input box for user to enter stock ticker
     ticker = st.text_input("Enter Stock Ticker (e.g., SPY, AAPL, TSLA):", value="SPY").upper()
 
+    st.write("testing if after ticker is OK")
     # Initialize session states
     if 'index' not in st.session_state:
         st.session_state.index = 0
