cc2638c7fa2d21800040651c5388434b084cbe2b
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-09 11:52:20 -0500

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -477,9 +477,9 @@ def main():
     # Calculate residuals for each row
     data_recent["residuals"] = y - y_pred_poly
     # Option 1: Rolling standard deviation (e.g., over 50 time points)
-   # data_recent["std_dev"] = data_recent["residuals"].rolling(window=30).std()## first 50 time points no data
+    data_recent["std_dev"] = data_recent["residuals"].rolling(window=30).std()## first 50 time points no data
 
-    #std_dev = data_recent["std_dev"].iloc[-1]
+    std_dev = data_recent["std_dev"].iloc[-1]
 
     # Determine the trend message
     if current_price > data_recent['EMA_9'].iloc[-1] and data_recent['EMA_9'].iloc[-1] > data_recent['EMA_20'].iloc[-1]:
