faf1f8f866d962816028322ecc571594c1b3642a
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-10 14:41:28 -0500

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -880,8 +880,8 @@ def main():
 
 ##########. try cloud for today
  
-    tfAll = [1,5,15,30]
-    if st.session_state.index <4 and st.session_state.index >0:
+    tfAll = [1,5,15,30,60]
+    if st.session_state.index <5 and st.session_state.index >0:
         time_frame = tfAll[st.session_state.index]  # Change this to 1, 5, 15, etc. (minutes per data point)
 
 # Simulated data covering from 20:00 (yesterday) to now
