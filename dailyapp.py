71d879af0f63198082a49e9efff1b1c368dd361f
codesareus <125232658+codesareus@users.noreply.github.com>
2025-03-10 09:09:41 -0500

    Update dailyapp.py

--- dailyapp.py
+++ dailyapp.py
@@ -883,14 +883,14 @@ def main():
     tfAll = [1,5,15,30,60,24*60,  24*60, 24*60, 24*60]
     time_frame = tfAll[st.session_state.index]  # Change this to 1, 5, 15, etc. (minutes per data point)
     data_recentNew = np.random.randn(24 * 60 // time_frame)  # Simulated data for a full day
-    x_values = np.arange(len(data_recentNew))  # Numeric x-axis
+    x_valuesNew = np.arange(len(data_recentNew))  # Numeric x-axis
 
 # Calculate index for 4 AM
     minutes_per_day = 24 * 60
     index_4am = (4 * 60) // time_frame  # Convert 4 AM into index based on time frame
 
 # Add grey shading from 4 AM onward
-    ax.axvspan(index_4am, len(data_recent) - 1, color='red', alpha=0.5)
+    ax.axvspan(index_4am, len(data_recentNew) - 1, color='gray', alpha=0.3)
 #####################
     # redeclare, messed up by above
     x_values = np.arange(len(data_recent))  # Numeric x-axis
